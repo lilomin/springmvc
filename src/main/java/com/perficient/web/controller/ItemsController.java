5537de801c439b56cbd26d04cd2fc533433f5c31
Raymond Li <396277832@qq.com>
2016-02-26 13:59:21 +0800

    Fixed URI bugs

--- src/main/java/com/perficient/web/controller/ItemsController.java
+++ src/main/java/com/perficient/web/controller/ItemsController.java
@@ -1,36 +1,18 @@
 package com.perficient.web.controller;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.mvc.Controller;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.perficient.pojo.Items;
+@Controller
+@RequestMapping("/")
+public class ItemsController{
 
-public class ItemsController implements Controller {
-
-	public ModelAndView handleRequest(HttpServletRequest request,
-			HttpServletResponse response) throws Exception {
-		String username = null;
-		if(request.getSession().getAttribute("currentUser") != null){
-			username = request.getSession().getAttribute("currentUser").toString();
-		}
-		List<Items> itemsList = new ArrayList<Items>();
-		Items item = new Items();
-		item.setName("Apple iphone");
-		item.setPrice(6088f);
-		item.setDetail("Apple iphone6s mobile phone");
-		itemsList.add(item);
-		
-		ModelAndView modelAndView = new ModelAndView();
-		modelAndView.addObject("itemsList", itemsList);
-		modelAndView.addObject("username", username);
-		modelAndView.setViewName("itemsList");
-		return modelAndView;
+	@RequestMapping("/items")
+	public String showItemsList(HttpServletRequest request, HttpServletResponse response, Model model){
+		return "itemsList";
 	}
-
 }
