02bda95085e79f8274d6c7cff3171067486786d4
raymond <396277832@qq.com>
2016-04-28 15:53:36 +0800

    updated sql

--- src/main/webapp/resources/sql/init.sql
+++ src/main/webapp/resources/sql/init.sql
@@ -1,17 +1,22 @@
-CREATE TABLE user (
-	id int(11) PRIMARY-KEY NOT NULL AUTO_INCREMENT,
+CREATE TABLE users (
+	id int NOT NULL AUTO_INCREMENT,
 	name varchar(20) DEFAULT NULL,
-	age int(11) DEFAULT NULL
-)
+	age int DEFAULT NULL,
+	PRIMARY KEY (id)
+)ENGINE=InnoDB CHARSET=utf8
 CREATE TABLE blogs (
-	id int(11) PRIMARY-KEY NOT NULL AUTO_INCREMENT,
+	id int NOT NULL AUTO_INCREMENT,
 	tilte varchar(200) NOT NULL,
 	content text DEFAULT NULL,
-	create_time DATE NOT NULL
-)
+	create_time DATE NOT NULL,
+	PRIMARY KEY (id)
+)ENGINE=InnoDB CHARSET=utf8
 CREATE TABLE commonts (
-	id int(11) PRIMARY-KEY NOT NULL AUTO_INCREMENT,
+	id int NOT NULL AUTO_INCREMENT,
 	commont varchar(100) NOT NULL,
-	blog_id int(11) NOT NULL,
-	user_id int(11) NOT NULL
-)
+	users_id int NOT NULL,
+	blogs_id int NOT NULL,
+	PRIMARY KEY (id),
+	FOREIGN KEY (users_id) REFERENCES users(id) ON DELETE CASCADE,
+	FOREIGN KEY (blogs_id) REFERENCES blogs(id) ON DELETE CASCADE
+)ENGINE=InnoDB CHARSET=utf8
